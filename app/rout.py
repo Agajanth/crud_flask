0ca44b8003985cd722151f0e3eb78b9bb0755113
David Santiago Isaza Arias <santiagoisaza0@gmail.com>
2021-06-23 14:23:52 -0500

    add a new rout to create a new register in the db with the model 'persona'

--- app/rout.py
+++ app/rout.py
@@ -1,7 +1,8 @@
 from app import app
 from app.database import *
-from flask import jsonify
+from flask import jsonify, Response,request
 from app.utils.parser import Parse
+
 @app.route('/all_registers',methods=['GET'])
 def all_registers():
     personas = Persona.query.all()
@@ -9,5 +10,17 @@ def all_registers():
     total = Persona.query.count()
     app.logger.debug(f"Listado de personas : {listado_personas}")
     return jsonify({"Total personas":total,"listado Personas":listado_personas})
+
+@app.route('/insertar_persona',methods=['POST'])
+def insertar_persona():
+    persona = Persona()
+    persona.nombre = request.json['name']
+    persona.email = request.json['email']
+    app.logger.debug(f"persona a insertar: nombre {persona.nombre} - email {persona.email}")
+
+    db.session.add(persona)
+    db.session.commit()
+    app.logger.debug(f"Usuario {persona} agregado con éxito")
+    return Response(jsonify({"Operacion registro":"realizada con exito"}),status=200)
     
     
